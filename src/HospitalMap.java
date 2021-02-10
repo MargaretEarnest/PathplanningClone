646f394a5860058b11646e8ca097236e9d0d9175
Othniel9 <64720103+Othniel9@users.noreply.github.com>
2021-02-10 09:30:39 -0500

    Update HospitalMap.java

--- src/HospitalMap.java
+++ src/HospitalMap.java
@@ -113,7 +113,8 @@ public class HospitalMap {
         StringBuilder edgeCSV = new StringBuilder();
         for (int i = 0; i < edges.size(); i++) {
 
-            edgeCSV = edgeCSV.append(edges.get(i).startNode + " , " + edges.get(i).endNode + " , " + edges.get(i).id + " , " +  '\n');
+            edgeCSV = edgeCSV.append(edges.get(i).id + " , " + edges.get(i).startNode + " , " + edges.get(i).endNote + " , " +  '\n');
+            edgeCSV = edgeCSV.append(edges.get(i).id + " , " + edges.get(i).endNode + " , " + edges.get(i).startnode + " , " +  '\n');
           //  System.out.println(edgeCSV);
 
 

ad40eff8bf8abd99491bfe24c222825a45a0891e
carleygilmore3@gmail.com <40573151+cgilmore18@users.noreply.github.com>
2021-02-13 02:24:40 -0500

    refactoring MapManager for paths

--- src/MapEditor.java
+++ src/MapEditor.java
@@ -1,9 +1,12 @@
-public class MapEditor {
+class MapEditor {
 	void addNode() {
 
 	}
-	void removeNode() {
+	void removeNode(HospitalMap.Node node) {
 
 	}
-	void edit
+	void editNode() {
+
+	}
+
 }

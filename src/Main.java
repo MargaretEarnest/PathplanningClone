074eadb029f7d3aad3abe4a4c1f0f7757c4cb4b6
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2021-02-13 01:23:57 -0500

    Made MapState

--- src/Main.java
+++ src/Main.java
@@ -14,41 +14,9 @@ import java.util.List;
 
 public class Main extends Application {
 
-	public int scale = 2; // scales image to 1/scale
 
 	public void start(Stage stage) throws IOException {
-		//MapPFaulkner1Nodes.csv
-		HospitalMap map = new HospitalMap();
-//    	map.generateElementFromData(ReadCSV.readFromFile("src/testmapNodes.csv"), ReadCSV.readFromFile("src/testmapEdges.csv"));
-		map.generateElementFromData(ReadCSV.readFromFile("src/MapPFaulkner1Nodes.csv"), ReadCSV.readFromFile("src/MapPFaulkner1Edges.csv"));
 
-//		System.out.println(DepthFirstSearch.performSearch(HospitalMap.nodesHash.get("N01"), HospitalMap.nodesHash.get("N13"), map));
-
-		//System.out.println(HospitalMap.nodes);
-
-		//creating the image object
-		InputStream stream = new FileInputStream("src/FaulknerFloor1.png");
-		Image image = new Image(stream);
-		//Creating the image view
-		ImageView imageView = new ImageView();
-		//Setting image to the image view
-		imageView.setImage(image);
-		//Setting the image view parameters
-		imageView.setX(0);
-		imageView.setY(0);
-		imageView.setFitWidth(image.getWidth() / scale);
-		imageView.setPreserveRatio(true);
-		//Setting the Scene object
-		Group root = new Group();
-		ImageHandler hander = new ImageHandler(HospitalMap.nodes, imageView, scale, root);
-		Scene scene = new Scene(root, image.getWidth() / scale, image.getHeight() / scale);
-		stage.setTitle("Floor Map");
-		stage.setScene(scene);
-		stage.show();
-
-		System.out.println(HospitalMap.nodeToCSV(HospitalMap.nodes));
-
-		System.out.println(HospitalMap.edgeToCSV(HospitalMap.edges));
 	}
 
 	public static void main(String args[])
